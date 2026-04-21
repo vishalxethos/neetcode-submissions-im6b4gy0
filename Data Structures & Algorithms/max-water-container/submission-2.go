f5f9238e8e2558cf46aaf37ed071c0c2bf00921c
vishal-singh <168514796+vishalxethos@users.noreply.github.com>
2026-04-21 23:05:54 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.go
@@ -0,0 +1,24 @@
+func maxArea(heights []int) int {
+        L,R := 0, len(heights) - 1
+
+        maxArea := 0
+
+        for L < R {
+                fmt.Println(L,R)
+                currentArea := min(heights[L],heights[R]) * (R - L)
+
+                maxArea = max(maxArea, currentArea)
+
+                if heights[L] <= heights[R] {
+                    L +=1
+                } else {
+
+                    R-=1
+
+                }
+        }
+
+        return maxArea
+
+
+}
